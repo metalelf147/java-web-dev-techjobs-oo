6550943524b2d9a9a45b3aef5edc058695b4ca15
Chan <chandler.eubank@gmail.com>
2020-12-21 19:54:05 -0600

    ok not sure but onto part 4 now

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,33 +1,38 @@
-package org.launchcode.techjobs_oo.Tests;
-import org.junit.Test;
-import org.junit.Before;
-import org.launchcode.techjobs_oo.Job;
-
-
-public class JobTest {
-
-    @Before
-    public void createJobObject() {
-        Job j1 = new Job();
-        Job j2 = new Job();
-    }
-
-
-    @Test
-    public void testSettingJobId() {
-
-    }
-
-
-//    Use assertEquals, assertTrue, or assertFalse to test that the ID values for the
-//    two objects are NOT the same and differ by 1.
+//package org.launchcode.techjobs_oo.Tests;
+//import org.junit.Test;
+//import org.junit.Before;
+//import org.launchcode.techjobs_oo.Job;
+//import org.junit.Assert;
+//import org.junit.After;
+//import static org.junit.Assert.assertTrue;
 //
-//    Run the test to verify that your Job() constructor correctly assigns ID numbers.
 //
-//    If the test doesn’t pass, what should be your first thought?
 //
-//            “Drat! I need to fix the unit test.”
-//            “Drat! I need to fix my Job() constructor code.”
-
-
-}
+//public class JobTest {
+//
+////    @Before
+////    public void createJobObject() {
+////        Job j1 = new Job();
+////        Job j2 = new Job();
+////    }
+//
+//    @Test
+//    public void testSettingJobId() {
+//        Job j1 = new Job();
+//        Job j2 = new Job();
+//        assertTrue(j1.getId() != j2.id);
+//    }
+//
+//
+////    Use assertEquals, assertTrue, or assertFalse to test that the ID values for the
+////    two objects are NOT the same and differ by 1.
+////
+////    Run the test to verify that your Job() constructor correctly assigns ID numbers.
+////
+////    If the test doesn’t pass, what should be your first thought?
+////
+////            “Drat! I need to fix the unit test.”
+////            “Drat! I need to fix my Job() constructor code.”
+//
+//
+//}
