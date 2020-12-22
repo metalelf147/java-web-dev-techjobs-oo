5a5118b35f23767e957a76112bfc03c2ac335aa4
Chan <chandler.eubank@gmail.com>
2020-12-21 19:35:00 -0600

    pre add junit4 to classpath test

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,33 @@
+package org.launchcode.techjobs_oo.Tests;
+import org.junit.Test;
+import org.junit.Before;
+import org.launchcode.techjobs_oo.Job;
+
+
+public class JobTest {
+
+    @Before
+    public void createJobObject() {
+        Job j1 = new Job();
+        Job j2 = new Job();
+    }
+
+
+    @Test
+    public void testSettingJobId() {
+
+    }
+
+
+//    Use assertEquals, assertTrue, or assertFalse to test that the ID values for the
+//    two objects are NOT the same and differ by 1.
+//
+//    Run the test to verify that your Job() constructor correctly assigns ID numbers.
+//
+//    If the test doesn’t pass, what should be your first thought?
+//
+//            “Drat! I need to fix the unit test.”
+//            “Drat! I need to fix my Job() constructor code.”
+
+
+}
